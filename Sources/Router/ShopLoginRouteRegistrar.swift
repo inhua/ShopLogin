edebf326b8508930e8da087fb56f63e67e44c8f2
2417524990@qq.com <2417524990@qq.com>
2026-05-01 18:06:29 +0800

    fix: remove initialize() - not permitted in Swift 5.3+

--- Sources/Router/ShopLoginRouteRegistrar.swift
+++ Sources/Router/ShopLoginRouteRegistrar.swift
@@ -1,13 +1,7 @@
-// ShopLoginRouteRegistrar.swift - ShopLogin 路由自注册
+// ShopLoginRouteRegistrar.swift - ShopLogin 路由注册
 import ShopRouter
 
 public final class ShopLoginRouteRegistrar: NSObject, MTRouteRegistrable {
-    override public class func initialize() {
-        super.initialize()
-        guard self === ShopLoginRouteRegistrar.self else { return }
-        registerRoutes()
-    }
-
     public static func registerRoutes() {
         MTRouter.shared.register(RouterPath.Login.main) { _ in LoginViewController() }
     }
